e056c90971b189bad8e7b760b98cd6cbfca14504
Patrick <patrickcabrera2025@gmail.com>
2026-04-14 18:03:15 -0500

    add os

---
+++ organizador/organizador.py
@@ -0,0 +1,11 @@
+import os
+
+#os.path.expanduser detecta automaticamente el usuario sin importar el pc donde esté "~"
+ruta = os.path.expanduser("~/Downloads")
+
+def obtener_tipo(extension) :
+    tipos = {
+        '.pdf' : 'PDFs',
+        '.jpg' : 'Imagenes',
+        '.jpeg' : 'Imagenes'
+    }
